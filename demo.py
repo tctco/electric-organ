355ae63468a241cf67f62f58c4887f954a231176
tctco <tctco001@163.com>
2020-01-17 16:25:42 +0800

    add some other functions and details

--- demo.py
+++ demo.py
@@ -1,4 +1,3 @@
-import time
 from kits import ScriptGenerator, generate_scale, load_sounds, Note
 from inputMethod import TextInput
 from threading import Thread
@@ -26,15 +25,43 @@ def draw_text(text, font_size, color, x, y):
 
 
 class Composer(TextInput):
-    note_list = []
-    begin_to_input = False
 
-    def character(self, key):
+    def __init__(
+            self,
+            initial_string='',
+            font_size=20,
+            font_family='microsoftyaheimicrosoftyaheiui',
+            antialias=True,
+            text_color=BLACK,
+            cursor_color=BLACK,
+            max_string_length=-1,
+            cursor_blink_interval=500,
+            keyboard_interval=50,
+            max_width=480,
+            max_height=600,
+    ):
+        super().__init__(initial_string,
+                         font_size,
+                         font_family,
+                         antialias,
+                         text_color,
+                         cursor_color,
+                         max_string_length,
+                         cursor_blink_interval,
+                         keyboard_interval,
+                         max_width,
+                         max_height)
+        self.note_list = []
+        self.begin_to_input = False
+        self.is_input_paused = False
+        self.pause_duration = 0
+
+    def notes(self, key):
         if not self.begin_to_input:
             self.begin_to_input = True
             self.start_time = pygame.time.get_ticks()
         if (len(self.input_string) < self.max_string_length or self.max_string_length == -1) and key in KEY_LIST:
-            note = Note(key, pygame.time.get_ticks() - self.start_time, None, note_dic[key])
+            note = Note(key, pygame.time.get_ticks() - self.start_time - self.pause_duration, None, note_dic[key])
             self.note_list.append(note)
 
     def status_check(self, key):
@@ -59,12 +86,8 @@ class Composer(TextInput):
             for i in range(-1, -len(self.note_list) - 1, -1):
                 if self.note_list[i].key == key:
                     note = self.note_list[i]
-                    note.duration = pygame.time.get_ticks() - self.start_time - note.start
-                    # s = "{}, {} ".format(note.start, note.duration)
-                    # self.input_string = self.input_string[:self.cursor_position] + s + \
-                    #                     self.input_string[self.cursor_position:]
-                    # self.cursor_position += len(s)
-                    self.note_list.sort(key = lambda note: note.start)
+                    note.duration = pygame.time.get_ticks() - self.start_time - note.start - self.pause_duration
+                    self.note_list.sort(key=lambda note: note.start)
                     self.input_string = ""
                     self.cursor_position = 0
                     for note in self.note_list:
@@ -79,21 +102,40 @@ class Composer(TextInput):
         self.key_status[key] = [False, 0]
         self.clock.tick()
 
+    def pause(self):
+        if not self.is_input_paused:
+            self.is_input_paused = True
+            self.paused_time = pygame.time.get_ticks()
+
+    def resume(self):
+        if self.is_input_paused and self.cursor_position == len(self.input_string):
+            self.is_input_paused = False
+            self.pause_duration += pygame.time.get_ticks() - self.paused_time
+
     def press_key(self, key):
         if key == pygame.K_LEFT:
+            self.pause()
             self.left()
         elif key == pygame.K_RIGHT:
+            self.pause()
             self.right()
         elif key == pygame.K_BACKSPACE:
+            self.pause()
             self.backspace()
         elif key == pygame.K_DELETE:
+            self.pause()
             self.delete()
         elif key in KEY_LIST:
-            self.character(key)
+            self.resume()
+            self.notes(key)
             return
         elif key == pygame.K_s and pygame.key.get_mods() & pygame.KMOD_CTRL:
+            self.pause()
             self.audition()
             return
+        elif pygame.K_0 <= key <= pygame.K_9 or key == pygame.K_COMMA:
+            self.pause()
+            self.character(key)
         self.key_init(key)
 
     def audition(self):
@@ -111,6 +153,7 @@ class UI:
     def __init__(self, selected=1):
         self.selected = selected
         self.piece = DEFAULT_PIECE_NAME
+        self.instrument = 1
 
     def draw(self):
         screen.fill(WHITE)
@@ -120,27 +163,38 @@ class UI:
         background_img.set_alpha(100)
         background_rect = background_img.get_rect()
         screen.blit(background_img, background_rect)
+        if self.instrument == 1:
+            instrument = "Guitar"
+        elif self.instrument == 0.8:
+            instrument = "drum"
+        elif self.instrument == 0.99:
+            instrument = "Between guitar and drum"
+
         if self.selected == 1:
-            draw_text(">>>Press a to z to play music<<<", UI_FONT_SIZE, BLACK, WIDTH / 2, 100)
+            draw_text(">>>{}<<<".format(instrument), UI_FONT_SIZE, BLACK, WIDTH / 2, 100)
         elif self.selected == 2:
             draw_text(">>>Play {}<<<".format(self.piece), UI_FONT_SIZE, BLACK, WIDTH / 2, 200)
         elif self.selected == 3:
             draw_text(">>>Choose a script<<<", UI_FONT_SIZE, BLACK, WIDTH / 2, 300)
         elif self.selected == 4:
             draw_text(">>>I want to compose<<<", UI_FONT_SIZE, BLACK, WIDTH / 2, 400)
-        draw_text("Press a to z to play music", UI_FONT_SIZE, BLACK, WIDTH / 2, 100)
+        draw_text("{}".format(instrument), UI_FONT_SIZE, BLACK, WIDTH / 2, 100)
         draw_text("Play {}".format(self.piece), UI_FONT_SIZE, BLACK, WIDTH / 2, 200)
         draw_text("Choose a script", UI_FONT_SIZE, BLACK, WIDTH / 2, 300)
         draw_text("I want to compose", UI_FONT_SIZE, BLACK, WIDTH / 2, 400)
 
     def draw_bar(self, pos, size, borderC, barC, progress):
         pygame.draw.rect(screen, borderC, (*pos, *size), 1)
-        innerPos = (pos[0] + 3, pos[1] + 3)
-        innerSize = ((size[0] - 6) * progress, size[1] - 6)
-        pygame.draw.rect(screen, barC, (*innerPos, *innerSize))
+        inner_pos = (pos[0] + 3, pos[1] + 3)
+        inner_size = ((size[0] - 6) * progress, size[1] - 6)
+        pygame.draw.rect(screen, barC, (*inner_pos, *inner_size))
 
     def function(self):
-        if self.selected == 2:
+        if self.selected == 1:
+            switch_instrument(self.instrument)
+            global note_dic
+            note_dic = load_sounds()
+        elif self.selected == 2:
             self.player_stop = False
             player = Thread(target=self.play_piece, args=(self.piece, lambda: self.player_stop))
             player.daemon = True
@@ -163,6 +217,19 @@ class UI:
             self.selected += 1
         elif key == pygame.K_RETURN:
             self.function()
+        elif key == pygame.K_RIGHT and self.selected == 1:
+            switch_sound.play()
+            if self.instrument == 0.8:
+                self.instrument = 0.99
+            elif self.instrument == 0.99:
+                self.instrument = 1
+        elif key == pygame.K_LEFT and self.selected == 1:
+            switch_sound.play()
+            if self.instrument == 1:
+                self.instrument = 0.99
+            elif self.instrument == 0.99:
+                self.instrument = 0.8
+
         self.draw()
 
     def compose(self, initial_string):
@@ -291,15 +358,21 @@ def get_input():
             pygame.display.flip()
 
 
+def switch_instrument(factor):
+    scale_generator = generate_scale(factor)
+    for progress in range(KEY_NUM):
+        screen.fill(BLACK)
+        draw_text("generating sounds{}".format('.' * (progress % 3 + 1)), UI_FONT_SIZE, WHITE, WIDTH / 2, HEIGHT * 0.7)
+        next(scale_generator)
+        ui.draw_bar(BAR_POS, BAR_SIZE, BORDER_COLOR, BAR_COLOR, (progress + 1) / 14)
+        pygame.display.flip()
+
+
 pygame.mixer.set_num_channels(14)
 ui = UI(1)
-scale_generator = generate_scale()
-for progress in range(KEY_NUM):
-    next(scale_generator)
-    ui.draw_bar(BAR_POS, BAR_SIZE, BORDER_COLOR, BAR_COLOR, (progress + 1) / 14)
-    pygame.display.flip()
-ui.draw()
+switch_instrument(1)
 note_dic = load_sounds()
+ui.draw()
 while 1:
     clock.tick(100)
     event_list = pygame.event.get()
